ff56e68d02dd6e029b0782aefd4a70bf22b28ac9
lemonjin1997 <53006700+lemonjin1997@users.noreply.github.com>
2021-12-02 16:33:36 +0800

    XSS check function updated

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
+import re
 
 app = Flask(__name__, template_folder='template')
 
@@ -43,7 +44,35 @@ def search():
 
 @app.route('/searchPost',  methods=['POST'])
 def searchPost():
-    Username = request.form.get('Username')
+    Search = request.form.get('Search')
+    if check_content():
+        pass
+
+
+# Check input content
+def check_content(data):
+    regex = r'''^[A-Za-z0-9,.!#$%&+\/=?^_`{|}~()"\- ]+$'''
+    regex2 = r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'''
+
+    temp_data = str(self.data).strip()
+
+    ptn = re.compile(regex)
+    ptn2 = re.compile(regex2)
+    
+    if len(temp_data) > 500:
+        print("fail 0")
+        return False
+
+    if not bool(ptn.search(temp_data)):
+        print("fail 1")
+        return False
+
+    elif bool(ptn2.search(temp_data)):
+        print("fail 2")
+        return False
+
+    else:
+        return temp_data
 
 if __name__== "__main__": 
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
